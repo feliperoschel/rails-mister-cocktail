94b2443d019275709b2225d5c4b42e5aadfe9d4f
Felipe Roschel <f.roschel@gmail.com>
2019-07-25 20:32:10 +0800

    controllers, seeds and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, shallow: true
+  end
+  # resources :doses, only: [:destroy]
+  # resources :doses, only: [:new, :create, :destroy]
+  root to: 'cocktails#index'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Dose.destroy_all if Rails.env.development?
+
+Ingredient.destroy_all if Rails.env.development?
+
+Cocktail.destroy_all if Rails.env.development?
+
+puts 'Creating Cocktails, Ingredients and Doses...'
+
+url = 'https://raw.githubusercontent.com/maltyeva/iba-cocktails/master/recipes.json'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+user.each do |recipe|
+  cocktail = Cocktail.find_or_create_by!(name: recipe['name'])
+
+  recipe['ingredients'].each do |i|
+    if i['label'].nil? ? (name = i['ingredient']) : (name = i['label'])
+      unless name.nil?
+        ingredient = Ingredient.find_or_create_by!(name: name)
+        Dose.find_or_create_by!(
+          description: "#{i['amount']}-#{i['unit']}",
+          ingredient_id: ingredient.id,
+          cocktail_id: cocktail.id
+        )
+      end
+    end
+  end
+end
+
+puts "Created #{Cocktail.count} cocktails!"
+puts "Created #{Ingredient.count} ingredients!"
+puts "Created #{Dose.count} doses!"
+
+# https://www.thecocktaildb.com/api.php
