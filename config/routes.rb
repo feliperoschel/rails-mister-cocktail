9fe4205f2ade2adaaab7c1e2dc3cc3619cd3889f
Felipe Roschel <f.roschel@gmail.com>
2019-07-25 20:59:28 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, shallow: true
+    # resources :doses, shallow: true
+    resources :doses, only: [:new, :create, :destroy]
   end
-  # resources :doses, only: [:destroy]
-  # resources :doses, only: [:new, :create, :destroy]
+  resources :doses, only: [:destroy]
   root to: 'cocktails#index'
 end
