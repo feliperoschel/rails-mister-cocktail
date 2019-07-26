9e207ac7d308b71f4a269367cebcf8d1ad191355
Felipe Roschel <f.roschel@gmail.com>
2019-07-26 16:52:08 +0800

    mr cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show]
+  before_action :set_cocktail, only: [:show, :edit, :update]
 
   def index
     @cocktails = Cocktail.all
